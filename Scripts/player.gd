9dc2ba1258a002a61711459c446701a7220a5d02
Rubén Piñeiro <rubenpifeinstituto@gmail.com>
2025-06-28 15:51:41 +0200

    add mech dash

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 10000
-var screen = get_viewport_rect().size
+var dir = Vector2.ZERO
 
-func _process(delta: float) -> void:
-	var dir = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
-	
+func _physics_process(delta: float) -> void:
+	dir = Input.get_vector("left","right","up","down")
+	dir = dir.normalized()
 	velocity = dir * speed * delta
-	
+	if Input.is_action_just_pressed("dash"):
+		dash()
+	print(dir)
 	move_and_slide()
+
+func dash():
+	$AnimationPlayer.play("move_dash")
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position", position + velocity * 1.5 ,0.5)
+	await tween.finished
 	
