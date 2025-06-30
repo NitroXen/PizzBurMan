2206fec2dca4f9bc33de3eb5ba0f435f1b3d7c03
Rubén Piñeiro <rubenpifeinstituto@gmail.com>
2025-06-30 17:55:01 +0200

    renew player

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends Area2D
 
 @export var speed = 200
 @export var dash_duration = 0.5
@@ -7,27 +7,29 @@ extends CharacterBody2D
 var is_dashing = false
 var dash_direction = Vector2.ZERO
 var dash_timer = 0.0
-
+var dir = Vector2.ZERO
+var vel = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	var dir = Input.get_vector("left","right","up","down")
+	dir = Input.get_vector("left","right","up","down")
 	if dir.length() > 0:
 		dir = dir.normalized()
 	if is_dashing:
 		dash_timer += delta
-		velocity = dash_direction * 600
+		vel = dash_direction * 600
 		$AnimationPlayer.play("move_dash")
 		if dash_timer > dash_duration:
 			dash_timer = 0.0
 			is_dashing = false
-			velocity = Vector2.ZERO
+			vel = Vector2.ZERO
 	else:
-		velocity = dir * speed
-	move_and_slide()
+		vel = dir * speed
+	position += vel*delta
+	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("dash"):
+	if event.is_action_pressed("dash") and dir != Vector2.ZERO:
 		is_dashing = true
 		dash_direction = Input.get_vector("left", "right", "up", "down")
 		if dash_direction.length() == 0:
