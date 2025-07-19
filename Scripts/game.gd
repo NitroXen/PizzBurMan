5d5f849fb429f402610e967eeba9757ae9b6c858
Rubén Piñeiro <rubenpifeinstituto@gmail.com>
2025-07-19 17:25:49 +0200

    add funcionality and gameplay

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,8 +1,12 @@
 extends Node
 
 func _process(delta: float) -> void:
+	if Autoload.puntuacion>0:
+		$Info.hide()
+	$Timer.text = str(int($EatingTimer.time_left))
 	$Label.text = "Puntos: %d" % Autoload.puntuacion
+	
 
 
 func _on_eating_timer_timeout() -> void:
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -5,9 +5,5 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 
-func _on_game_button_pressed() -> void:
-	pass
-	
-
-func _on_info_button_pressed() -> void:
-	pass # Replace with function body.
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
